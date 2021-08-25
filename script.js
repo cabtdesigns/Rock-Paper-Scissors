e52fae2e0339d03f9cbd6beb95dce81a3e01548b
Chris <chris@cabtdesigns.com>
2021-08-25 01:28:53 -0700

    planned out program

---
+++ script.js
@@ -0,0 +1,23 @@
+// making a rock paper scissor game(RPS). will be played in the console
+
+//create a function called computerPlay
+
+//write a function that plays one round of RPS
+//should have 2 parameter playerSelection and computerSelection
+//then a return string that declares a winner. like you lose, paper beats rock
+//make parameter case sensitive so you can use rock,ROCK, RoCk etc
+
+// write a new function called game().
+//have it play 5 rounds
+//have it keep score
+//have it tell you the winner and loser at the end
+//can use a loop or call the playRound() function 5 times
+//use prompt to get user input
+
+function playRound(playerSelection, computerSelection) {
+    // your code here!
+  }
+  
+  const playerSelection = "rock";
+  const computerSelection = computerPlay();
+  console.log(playRound(playerSelection, computerSelection));
