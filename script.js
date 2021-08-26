44ca614715fd99e3ebd5397fbc55bc6b32410b8e
Chris <chris@cabtdesigns.com>
2021-08-25 19:29:47 -0700

    user and computer respone being stored in variable

--- script.js
+++ script.js
@@ -13,16 +13,14 @@ function game(){
 //have it keep score
 //have it tell you the winner and loser at the end
 //can use a loop or call the playRound() function 5 times
-
+let userResponse = null;
 //use prompt to get user input
 function userInput() {
     let userPick = prompt("Want to play a game? Pick rock, paper or scissor", "");
 //make parameter case sensitive so you can use rock,ROCK, RoCk etc
     let userPickToLowercase = userPick.toLowerCase();
 //check to see if they put in a correct type of choice
-
-    let choice = userPickToLowercase;
-if (choice == "") {
+if (userPickToLowercase == "") {
         alert("Please fill in an answer");
         userInput();
         return;
@@ -31,8 +29,7 @@ if (choice == "") {
         userInput();
         return;
     } else if (userPickToLowercase == "rock" || userPickToLowercase == "paper" || userPickToLowercase == "scissor"){
-        console.log(choice);
-        return;
+        userResponse = userPickToLowercase; 
     }
     
 }
@@ -42,21 +39,24 @@ if (choice == "") {
 //should have 2 parameter playerSelection and computerSelection
 //then a return string that declares a winner. like you lose, paper beats rock
 //create a function called computerPlay that will randomly pick RP or S
+let computerResponse = null;
+
 function computerPlay() {
     let computerChocies = ["Rock", "Paper", "Scissor"];
     let randomNumber = Math.floor(Math.random()*computerChocies.length);
     let computersPick = computerChocies[randomNumber];
+    computerResponse = computersPick;
     return computersPick;
 }
 
 
-function playRound(playerSelection, computerSelection) {
-    userInput();
-    let = ComputerPlay = "paper"
-    if (userInput == "rock" && ComputerPlay == "paper")
-    alert("computer wins")
-  }
-  
-  let playerSelection = "rock";
-  const computerSelection = computerPlay();
-  console.log(playRound(playerSelection, computerSelection));
+function playRound() {
+  // your code here!
+  userInput();
+  computerPlay();
+  console.log(userResponse + "|" + computerResponse)
+}
+
+const playerSelection = "userInput";
+const computerSelection = computerPlay();
+//console.log(playRound(playerSelection, computerSelection));
